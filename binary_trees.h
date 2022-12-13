fc42bc25c7014b055580528ac6b943b211a35fda
Mak655 <mekdemyoseph@gmail.com>
2022-12-13 13:41:02 -0800

    king

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,18 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+/* basic binary tree */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -18,38 +29,18 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-/**
- * struct queue_n_s - A queue node
- *
- * @value: Pointer to a tree node
- * @next: Pointer to the next tree node
- */
-struct queue_n_s
-{
-	binary_tree_t *value;
-	struct queue_n_s *next;
-};
-typedef struct queue_n_s queue_n;
-
-/**
- * struct queue_s - A queue data structure
- *
- * @head: Pointer to head of the queue
- * @tail: Pointer to tail of the queue
- */
-struct queue_s
-{
-	queue_n *head;
-	queue_n *tail;
-};
-typedef struct queue_s queue_t;
-
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -69,8 +60,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(
-	const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -83,13 +74,21 @@ bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+void binary_tree_print(const binary_tree_t *);
 
-/* Helpers */
+avl_t *rebalance(avl_t *node, avl_t **tree);
+avl_t *balance_left(avl_t *node);
+avl_t *balance_right(avl_t *node);
+char *convert(unsigned long int num, int base, int lowercase);
+bst_t *swap(bst_t *a, bst_t *b);
+
 size_t _binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_left_most_depth(const binary_tree_t *tree);
-int _is_perfect(size_t req_depth, size_t depth, const binary_tree_t *tree);
-queue_t *insert_to_queue(queue_t **queue, const binary_tree_t *tree);
-binary_tree_t *pop_from_queue(queue_t **queue);
 
-
-#endif
+#endif /* BINARY_TREES_H */
